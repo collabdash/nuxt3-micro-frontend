c5c01c5d8e30963d1c8d8c0c4ec06750128e14ab
Eulier <eulier@toolboxos.com>
2022-06-23 20:53:29 -0300

    define layout for store

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout name="default">
+      Nuxt Main App
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Main Layout
+    <br>
+    store data
+    <br>
+    {{ store.$state }}
+    <br>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useStore } from '@/stores/store'
+
+const store = useStore()
+
+</script>
