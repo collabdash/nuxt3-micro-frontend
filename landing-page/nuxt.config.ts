e6861cc3caf398f39551d57072d51204d4bc9789
Eulier <eulier@toolboxos.com>
2022-06-28 12:28:30 -0300

    subapp-config: 1. add & set nuxt plugin

--- landing-page/nuxt.config.ts
+++ landing-page/nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@femessage/nuxt-micro-frontend',
+    ],
+    MFE: {
+        force: true
+    }
 })
