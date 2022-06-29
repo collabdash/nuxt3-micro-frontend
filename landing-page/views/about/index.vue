3a6ca4e54fd0332c0ab484467731ba0dce8fa460
Eulier <eulier@toolboxos.com>
2022-06-29 19:21:53 -0300

    subapp-config: 3. set views & routes

---
+++ landing-page/views/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h2>About page (from Landing Microapp)</h2>
+    <button @click="go2HomePage">Go to Home</button>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const go2HomePage = () => router.push({ name: "home" })
+
+</script>
+
+<style>
+
+</style>
