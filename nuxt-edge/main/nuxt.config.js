0d9555178f3ee0e2818fd9f3f58efae9ec6e938c
Eulier <eulier@toolboxos.com>
2022-07-01 11:10:59 -0300

    migrate from Nuxt 2 to Nuxt-bridge

--- nuxt-edge/main/nuxt.config.js
+++ nuxt-edge/main/nuxt.config.js
@@ -1,4 +1,6 @@
-export default {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
@@ -28,8 +30,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
@@ -39,4 +39,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+})
